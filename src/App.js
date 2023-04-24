d12ee4160d6952f0599d3a99ebba8b7e2cdd36a4
unknown <eliyahulevinson@gmail.com>
2023-04-24 19:33:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />   
+    </>
+  );
+}
+
+export default App;
